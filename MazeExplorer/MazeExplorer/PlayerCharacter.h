7f078248fbe97c10a9218041791ebdebaa9b35e7
Marjorie <mprincip8336@scrippscollege.edu>
2014-02-20 00:05:58 -0800

    Added PlayerCharacter class and a small thing that reacts to touch (I hope).

---
+++ MazeExplorer/MazeExplorer/PlayerCharacter.h
@@ -0,0 +1,13 @@
+//
+//  PlayerCharacter.h
+//  MazeExplorer
+//
+//  Created by CS121 on 2/19/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlayerCharacter : NSObject
+
+@end
---
+++ MazeExplorer/MazeExplorer/PlayerCharacter.m
@@ -0,0 +1,13 @@
+//
+//  PlayerCharacter.m
+//  MazeExplorer
+//
+//  Created by CS121 on 2/19/14.
+//
+//
+
+#import "PlayerCharacter.h"
+
+@implementation PlayerCharacter
+
+@end
