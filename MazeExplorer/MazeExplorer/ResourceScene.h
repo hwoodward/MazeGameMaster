0b3183e31801435415854fecda60b1c977805a17
Emily Stansbury <emilystansbury@mac.com>
2014-03-01 19:27:22 -0800

    Split the screen between maze and resource scenes

---
+++ MazeExplorer/MazeExplorer/ResourceScene.h
@@ -0,0 +1,13 @@
+//
+//  ResourceScene.h
+//  MazeExplorer
+//
+//  Created by Emily Stansbury on 3/1/14.
+//
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface ResourceScene : SKScene
+
+@end
---
+++ MazeExplorer/MazeExplorer/ResourceScene.m
@@ -0,0 +1,13 @@
+//
+//  ResourceScene.m
+//  MazeExplorer
+//
+//  Created by Emily Stansbury on 3/1/14.
+//
+//
+
+#import "ResourceScene.h"
+
+@implementation ResourceScene
+
+@end
