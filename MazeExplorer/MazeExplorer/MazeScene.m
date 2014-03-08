18e6bdb0b24d987def5a31816ead61b3dee2d534
Marjorie <mprincip8336@scrippscollege.edu>
2014-03-08 14:08:58 -0800

    Oh thank goodness it stopped crashing.

--- MazeExplorer/MazeExplorer/MazeScene.m
+++ MazeExplorer/MazeExplorer/MazeScene.m
@@ -337,16 +337,6 @@ static const int CELLNUM = 11;
         //This should call the function in MyScene that calls the function in ResourceScene that increases the counter.
         if (![cont compare:@"R"] && didMove) {
             [self tellMySceneToIncreaseResourceCounter];
-            /*
-            //Change the MazeCell to have an empty space
-            MazeCell* resourceCell =[_maze returnCellWithRow: _playerLoc.x andColumn: _playerLoc.y];
-            resourceCell.contents = @" ";
-            //After that, I can figure out how to update the map (or at least recolor that one).
-            NSArray * nodesAtCurrentPos = [self nodesAtPoint: _player.position];
-            SKSpriteNode * resourceNode = nodesAtCurrentPos[0];
-            resourceNode.color = [SKColor whiteColor];
-             */
-
         }
         
         didMove = NO; 
