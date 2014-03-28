f8bd84d1dd6e56b7b079e4a9cb807fd05846c7e1
Marjorie <mprincip8336@scrippscollege.edu>
2014-03-27 23:15:57 -0700

    Added a new class for the confirmation message.

---
+++ MazeExplorer/MazeExplorer/ResourceConfirm.h
@@ -0,0 +1,13 @@
+//
+//  ResourceConfirm.h
+//  MazeExplorer
+//
+//  Created by CS121 on 3/27/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ResourceConfirm : NSObject
+
+@end
---
+++ MazeExplorer/MazeExplorer/ResourceConfirm.m
@@ -0,0 +1,13 @@
+//
+//  ResourceConfirm.m
+//  MazeExplorer
+//
+//  Created by CS121 on 3/27/14.
+//
+//
+
+#import "ResourceConfirm.h"
+
+@implementation ResourceConfirm
+
+@end
